516399017fc97adec637ae5c03ef8df2f7316f1c
n0x it <johnny.trucolo97@gmail.com>
2022-06-19 19:56:00 -0300

    history has been done

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,28 +5,21 @@ function avancarImagem(){
     imagem = imagens[proximaImagem];
     imagem.style.zIndex = ++zIndexAtual;
     imagem.style.marginLeft = "0%";
-
     proximaImagem++;
     if(proximaImagem >= imagens.length){
         proximaImagem = 0;
     }
-
     setTimeout(resetarImagens,500);
 }
-
 function resetarImagens(){
     imagemVisivel = proximaImagem -1;
-
-    if(imagemVisivel < 0){
-        
+    if(imagemVisivel < 0){    
         imagemVisivel = imagens.length - 1;
     }
-
     for(var i = 0; i < imagens.length; i++){
         if(i != imagemVisivel){
             imagens[i].style.marginLeft = "100%";
         }
     }
 }
-
 var intervalo = setInterval(avancarImagem,2000);
