17590f88d4ef329da4773e7ebb6eb1f1620a115b
n0x it <johnny.trucolo97@gmail.com>
2022-06-18 21:56:29 -0300

    landing page is done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # granulando-art
+
+## lading page has been done
+logo, unorder list, script, articles, footer.
---
+++ scripts/sr.js
@@ -0,0 +1,4 @@
+ScrollReveal({ distance: '60px' });
+ScrollReveal().reveal('.news', { origin: 'bottom' });
+ScrollReveal().reveal('.news', { origin: 'top' });    
+ScrollReveal().reveal('.news', {delay: 200});
